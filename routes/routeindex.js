b878a99975302f80b363abb126ca611abe30cb4c
Rodrigo <A01651416@itesm.mx>
2021-07-28 15:09:50 -0500

    crea profe ya funciona

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -4,12 +4,22 @@ const Prof = require('../model/profesores');
 
 
 // Nos regresaria las tareas guardadas en la BD con el método find(). Una vez obtenidas las tareas las regresamos a la pagina principal.
-app.get('/', async function(req,res){
+app.get('/', async (req,res)=>{
     var pr = await Prof.find(); 
     console.log(pr); 
     res.render("index",{pr}); 
 });
 
+app.get('/crea-profe', (req, res)=>{
+    res.render("crea-profe")
+}); 
+
+
+app.post('/add', (req,res)=>{
+    var pr = new Prof(req.body); 
+    pr.save(); 
+    res.redirect('/'); 
+}); 
 
 // toma a profesor seleccionado y despliega su info en una página con ruta dinámica
 app.get("/ve-profe/:id", async (req,res)=>{
@@ -18,6 +28,8 @@ app.get("/ve-profe/:id", async (req,res)=>{
     res.render('ve-profe', {pr})
 }); 
 
+
+//delete de elemento en base de datos
 app.get("/delete/:id", async (req,res)=>{
     var id = req.params.id; 
     await Prof.remove({_id: id});
