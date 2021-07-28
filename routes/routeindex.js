45e5e630281a148d2a23ecc675a0cc6cb41efbc4
Rodrigo <A01651416@itesm.mx>
2021-07-28 00:31:33 -0500

    2 paginas nuevas y pocos cambios

--- routes/routeindex.js
+++ routes/routeindex.js
@@ -5,8 +5,15 @@ const Task = require('../model/task');
 
 // Nos regresaria las tareas guardadas en la BD con el método find(). Una vez obtenidas las tareas las regresamos a la pagina principal.
 app.get('/', async function(req,res){
-    res.send("hola"); 
+    res.render("index"); 
 });
+
+
+// toma a profesor seleccionado y despliega su info en una página con ruta dinámica
+app.get("/ve-profe:profeSelec", async (req,res)=>{
+    var chosen = req.params.profeSelec; 
+    res.render("ve-profe")
+}); 
 
 
 module.exports = app;
