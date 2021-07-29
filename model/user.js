1219585e2fc3b545f595fd700c2e589aa0db7957
IsaacIpenza <benjamin.ipre@gmail.com>
2021-07-29 11:37:55 -0500

    Avance 28/07/2021 para manejo de usuarios

---
+++ model/user.js
@@ -0,0 +1,22 @@
+var mongoose = require("mongoose")
+var Schema = mongoose.Schema;
+var bcrypt = require("bcrypt");
+
+var UserSchema = Schema ({
+    username: String,
+    email: String,
+    password: String,
+    admin: Boolean,
+    default: false
+    
+});
+
+UserSchema.methods.encryptPassword = function(password) {
+    return bcrypt.hashSync(password, 10) ;   
+}
+
+UserSchema.methods.validatePassword = function(userPassword) {
+    return bcrypt.compare(userPassword, this.password) ;   
+}
+
+module.exports = mongoose.model('users', UserSchema);
--- routes/routeindex.js
+++ routes/routeindex.js
@@ -1,15 +1,46 @@
 const express = require('express');
 const app = express();
 const Prof = require('../model/profesores');
+const User = require('../model/user')
 
 
 // Nos regresaria las tareas guardadas en la BD con el método find(). Una vez obtenidas las tareas las regresamos a la pagina principal.
+
+//=================== Home ===================
+//Este home inicial es para todo el mundo en general, hayas o no iniciado secion
 app.get('/', async (req,res)=>{
+    var pr = await Prof.find(); 
+    console.log(pr); 
+    res.render("home",{pr}); 
+});
+
+//Este index es para agregar/editar/eliminar tus comentarios
+app.get('/index', async (req,res)=>{
     var pr = await Prof.find(); 
     console.log(pr); 
     res.render("index",{pr}); 
 });
 
+
+//=================== Usuarios ===================
+app.get('/login', function(req,res){
+    res.render('login');
+  })
+
+app.get('/register', function(req,res){
+    res.render('register');
+})
+
+app.post('/addUser',async function(req,res){
+  var user = new User(req.body);
+  user.password = user.encryptPassword(user.password);
+
+  await user.save();
+  res.redirect('/login');
+})
+
+
+//=============================================
 app.get('/crea-profe', (req, res)=>{
     res.render("crea-profe")
 }); 
@@ -17,7 +48,10 @@ app.get('/crea-profe', (req, res)=>{
 
 app.post('/add', async (req,res)=>{
     var pr = new Prof(req.body); 
-    pr.save(); 
+    //TODO: Falta agregar el verify-->middleware/verifyAccess
+    //pr.user_id = req.userId;
+
+    await pr.save(); 
     res.redirect('/'); 
 }); 
 
